5306a87ca086b9001e718fa60fb430c59f549ade
Rediet-Ferew <redietasnakech@gmail.com>
2022-09-11 15:13:56 +0300

    Editing printf file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
-     * main - Prints onto screen
-     * 
-     *  Return: zero
-              */
+* main - prints onto the screen
+*
+* Return: zero
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
 	return (0);
 }
