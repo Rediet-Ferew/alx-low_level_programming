8042c0cab5750f688eaf214132d6e35e4d4d4b40
Rediet-Ferew <redietasnakech@gmail.com>
2022-09-11 13:16:35 +0300

    Returning the last digit of a number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - returns the last digit of a number
+ * 
+ * Return: zero 
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int last = n%10;
+    char str[] = "Last digit of";
+
+    if (last > 5)
+    {
+        printf("%s %d is %d and is greater than 5", str, n, last);
+    } else if (last < 6 && last != 0){
+        printf("%s %d is %d and is less than 6 and not 0", str, n, last);
+    } else if(last == 0){
+        printf("%s %d is %d and is 0", str, n, last);
+    }
+	return (0);
+}
