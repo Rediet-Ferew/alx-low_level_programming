97576caf6c893f4b771aed064a09579c1a4fee3c
Rediet-Ferew <redietasnakech@gmail.com>
2022-09-11 13:22:01 +0300

    Returning the last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int n;
     int last;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	last = n%10;
     char str[] = "Last digit of";
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    last = n%10;
 
     if (last > 5)
     {
