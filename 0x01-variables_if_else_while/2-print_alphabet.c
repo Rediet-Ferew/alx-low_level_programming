07e5c76a65ca6436c321724689d1ca195bdbbe6c
Rediet-Ferew <redietasnakech@gmail.com>
2022-09-11 14:07:20 +0300

    Returning the last digit of a number

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints lower case of letter
- * 
- * Return: zero
- */
+* main - printing lowercase letters
+*
+* Return: zero
+*/
 int main(void)
 {
-    char ch = 'a';
+	char ch = 'a';
 
-    for (ch = 'a'; ch <= 'z'; ch++)
-    {
-        putchar (ch);
-    }
-    putchar ('\n');
-    return(0);
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar (ch);
+	}
+	putchar ('\n');
+	return (0);
 
 }
